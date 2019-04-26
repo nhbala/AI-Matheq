39d42595172e16d4ab329166a0e8fe421f5bf2c6
Michael Li <michaelli@dhcp-ccc-8651.redrover.cornell.edu>
2019-04-26 10:45:07 -0400

    where did this code come from

--- nn.py
+++ nn.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 
 #function takes in parameter layer_number_list which is a list of layers
@@ -7,7 +8,7 @@ def create_neural_network(layer_number_list):
     final_lst = {}
     for i in range(len(layer_number_list)):
         if i == 0:
-            final_lst["l" + str(i)] = np.zeros(layer_number_list[i], 1)
+            final_lst["l" + str(i)] = np.zeros((layer_number_list[i], 1))
         else:
             final_lst["l" + str(i)] = np.array([])
         if i < len(layer_number_list) -1:
@@ -16,7 +17,7 @@ def create_neural_network(layer_number_list):
 
 #mapping label to matrix representation
 def map_label(symbol):
-    matrix = np.zeros(14, 1)
+    matrix = np.zeros((14, 1))
     if symbol == "\d":
         matrix[eval(symbol)][0] = 1.0
     elif symbol == '+':
@@ -30,11 +31,13 @@ def map_label(symbol):
     return matrix
 
 
-def sigmoid_function(input):
+def sigmoid_function(i):
+    #print i
+    #print math.e**(-i)
     return 1/(1 + math.e**(-i))
 
-def sigmoid_function_derivative(input):
-    return sigmoid(input) * (1 - sigmoid_function(input))
+def sigmoid_function_derivative(i):
+    return sigmoid_function(i) * (1 - sigmoid_function(i))
 
 
 def training_function(t_data, nn, run_amt, l_rate, ac_func, der_ac_func):
@@ -53,14 +56,14 @@ def training_function(t_data, nn, run_amt, l_rate, ac_func, der_ac_func):
             layer0 = nn["l0"]
             for n in range(len(t_data[t])-1):
                 new_n = n + 1
-                layer0[n] = t_data[t, new_n]
+                layer0[n] = t_data[t, new_n]/255
             nn["l0"] = layer0
 
             #saving inj values for later
             #a_j values are stored in neural network at each layer
-            inj_arr = []
+            inj_arr = (nn['l0']).tolist()
             #set activation energy for other layers
-            for i in (len(nn.getKeys()) -1)/2:
+            for i in range((len(nn.keys()) -1)/2):
                 vfunc = np.vectorize(ac_func)
                 prev_layer = nn["l" + str(i)]
                 weights = nn["w" + str(i)]
@@ -77,7 +80,8 @@ def training_function(t_data, nn, run_amt, l_rate, ac_func, der_ac_func):
             delta_j_arr = []
             #backward algorithm
             #finding delta_j values for output layer
-            final_layer = nn["l" + (len(nn.getKeys())-1)
+            final_layer = nn["l" + str((len(nn.keys()) -1)/2)]
+            #print inj_arr
             inj_values = inj_arr.pop()
             mse_error = 0
             for k in range(len(final_layer)):
@@ -85,21 +89,27 @@ def training_function(t_data, nn, run_amt, l_rate, ac_func, der_ac_func):
                 delta_j = der_ac_func(curr_inj) * ((map_label(t_data[t][0]))[k][0] - final_layer[k][0])
                 mse_error += (delta_j ** 2)
                 delta_j_arr.append(delta_j)
-            all_delta_j_arr.append(delta_j_arr)
+            all_delta_j_arr.append(delta_j_arr) #should we include delta J?
+            print str(t) + ": " + str(mse_error)
             total_mse += mse_error
 
 
+
             #this is the layer number for second to last layer/last weight
-            curr_start = ((len(nn.getKeys()) -1)/2) - 1
-            for l in range(curr_start, -1, -1):
+            #print inj_arr
+            curr_start = ((len(nn.keys()) -1)/2) - 1
+            for l in range(curr_start, 0, -1): #right now stop before first layer, should we include?
                 delta_i_arr = []
-                curr_layer = nn["l" + l]
-                cur_ini_values = inj_arr.pop()
+                curr_layer = nn["l" + str(l)]
+                if l != 0:
+                    curr_ini_values = inj_arr.pop()
+                else:
+                    curr_ini_values = inj_arr
                 #Michael did the code from here pls check this over
-                for m in range(len(curr_layer)):
+                for m in range(len(nn['w' + str(l)])):
                     sum_weight_deltaj = 0
                     for p in range(len(delta_j_arr)):
-                        sum_weight_deltaj = sum_weight_deltaj + nn["w" + l][m][p] * delta_j_arr[p]
+                        sum_weight_deltaj = sum_weight_deltaj + nn["w" + str(l)][m][p] * delta_j_arr[p]
                     curr_ini = curr_ini_values[m][0]
                     delta_i = der_ac_func(curr_ini) * sum_weight_deltaj
                     delta_i_arr.append(delta_i)
@@ -107,13 +117,16 @@ def training_function(t_data, nn, run_amt, l_rate, ac_func, der_ac_func):
                 delta_j_arr = delta_i_arr
 
             #update each weight
-            for q in range(curr_start):
-                curr_weights = nn["w" + q]
-                curr_layer = mm["l" + q]
-                for r in range(curr_weights):
-                    for s in range(curr_weights[r]):
-                        curr_weights[r][s] = curr_weights[r][s] + l_rate * curr_layer[r] * all_delta_j_arr[q][s]
+            for q in range(curr_start+1):
+                curr_weights = nn["w" + str(q)]
+                curr_layer = nn["l" + str(q)]
+                #print all_delta_j_arr
+                for r in range(len(curr_weights)):
+                    for s in range(len(curr_weights[r])):
+                        #print l_rate * curr_layer[s] * all_delta_j_arr[q][r]
+                        curr_weights[r][s] = curr_weights[r][s] + l_rate * curr_layer[s] * all_delta_j_arr[q][r]
 
-        mse_lst.append(total_mse)
+        #mse_lst.append(total_mse)
+        print '###################################\n' + str(total_mse) + '\n##########################'
 
-    return (nn, mse_lst)
+    #return (nn, mse_lst)
--- start.py
+++ start.py
@@ -1,14 +1,19 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as pt
+import nn
 
 
 
 data = pd.read_csv("train.csv").as_matrix()
 xtrain = data[0:21000, :]
 train_label = data[0:21000, 0]
-print(xtrain)
 
+
+curr_nn = nn.create_neural_network([784, 14])
+
+nn.training_function(xtrain, curr_nn, 25, .25, nn.sigmoid_function, nn.sigmoid_function_derivative)
+#print(results)
 # print(d)
 
 #
