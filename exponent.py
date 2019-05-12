e4e59aec7abc8615622919aacaaf4059e304fde1
nhbala <30578709+nhbala@users.noreply.github.com>
2019-05-11 21:58:52 -0400

    Add files via upload

--- exponent.py
+++ exponent.py
@@ -28,19 +28,33 @@ sample3 = [
 [2050, 293, 116, 240]
 ]
 
-def cluster(coor_lst):
-    X = np.zeros((len(coor_lst),2))
-    for i in range(len(coor_lst)):
-        X[i][0] = coor_lst[i][1] + coor_lst[i][3]
-        X[i][1] = 0
-    print(X)
-    km = KMeans(n_clusters=2,
-    init='random',
-    n_init=10,
-    max_iter=300,
-    tol=1e-04,
-    random_state=0)
-    y_km = km.fit_predict(X)
-    return y_km
+def expo(coor_lst):
+    expo_lst = [0]
+    for i in range(1,len(coor_lst)):
+        lowBound = coor_lst[i][1] + coor_lst[i][3]
+        midL = coor_lst[i-1][1] + (coor_lst[i-1][3]/2)
+        midR = midL
+        if i < len(coor_lst)-1:
+            midR = coor_lst[i+1][1] + (coor_lst[i+1][3]/2)
+        if lowBound < midL or lowBound < midR:
+            expo_lst.append(1)
+        else:
+            expo_lst.append(0)
+    return expo_lst
+
+# def cluster(coor_lst):
+#     X = np.zeros((len(coor_lst),2))
+#     for i in range(len(coor_lst)):
+#         X[i][0] = coor_lst[i][1] + coor_lst[i][3]
+#         X[i][1] = 0
+#     print(X)
+#     km = KMeans(n_clusters=2,
+#     init='random',
+#     n_init=10,
+#     max_iter=300,
+#     tol=1e-04,
+#     random_state=0)
+#     y_km = km.fit_predict(X)
+#     return y_km
 
 # cluster(sample3)
